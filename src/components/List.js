3a666d10de2a97c362baa8e43798e7c8d9d68d74
Abdul Ghofur <abdulghoin@gmail.com>
2019-07-16 12:06:05 +0700

    list page

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import ListItem from './ListItem';
 
 import './List.css';
 
 export default function() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://api.github.com/users');
+      const data = await res.json();
+
+      setData(data);
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <ul>
-      <ListItem />
+      {data.map(({node_id, avatar_url, login}) => (
+        <ListItem key={node_id} {...{avatar_url, login}} />
+      ))}
     </ul>
   );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function() {
+import './ListItem.css';
+
+export default function({avatar_url, login}) {
+  console.log(avatar_url);
+  return (
+    <li>
+      <img src={avatar_url} alt={login} />
+      <span>{login}</span>
+    </li>
+  );
   return <li>Item</li>;
 }
